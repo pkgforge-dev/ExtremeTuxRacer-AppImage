08e859e5b6eceb2f8f7bbd6e8358ba511a98e1f4
Link4Electronics <link4electronics@gmail.com>
2026-08-17 19:01:25 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,10 +3,10 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q extremetuxracer | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q extremetuxracer | awk '{print $2; exit}')
 export ARCH VERSION
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
+export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/pixmaps/etr.png
 export DESKTOP=/usr/share/applications/net.sourceforge.extremetuxracer.desktop
@@ -17,8 +17,6 @@ export PATH_MAPPING='/usr/share/etr:${SHARUN_DIR}/share/etr'
 quick-sharun \
 	/usr/bin/etr \
 	/usr/share/etr
-
-# Additional changes can be done in between here
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
